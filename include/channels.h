4aa0a17f21156666dd78e88ea9e32ae93f03980c
oogooro <70372978+oogooro@users.noreply.github.com>
2026-08-16 11:31:12 +0200

    Tweak sensor reading values

--- include/channels.h
+++ include/channels.h
@@ -2,12 +2,12 @@
 #include <Arduino.h>
 #include <config.h>
 
-#define SENSOR_POOLINGRATE 120
-#define SENSOR_THRESHOLD 40
+#define SENSOR_POOLINGRATE 140
+#define SENSOR_THRESHOLD 38
 #define SENSOR_MAX_VALUE 4095
 #define SENSOR_MAX_OVERHEAD 50
 #define NUM_OF_CHANNELS 8
-#define SENSOR_OVERSAMPLING 5
+#define SENSOR_OVERSAMPLING 6
 #define MAP(x) (min(map(pots[x], 0, SENSOR_MAX_VALUE - SENSOR_MAX_OVERHEAD, 0, 100), 100L))
 
 extern uint8_t potsMapped[NUM_OF_CHANNELS];
